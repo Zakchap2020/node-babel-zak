0324c674f0fe0e853fba84612a70edffcd15ece1
Zakari Chapman <zakarichapman@developersinstitutel.co.nz>
2021-02-24 09:52:23 +1300

    replaced books with marketplaces

---
+++ src/models/marketPlaceModel.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from 'mongoose';
+const { schema } = mongoose;
+
+const model = new Schema({
+  name: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  owner: String,
+  admins: String,
+});
+
+export default mongoose.model('Books', model, 'books');
